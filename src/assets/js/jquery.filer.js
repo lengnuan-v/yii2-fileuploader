53eec801a8b337fa6113185dfe46286f10b108ef
lengnuan-v <45681358+lengnuan-v@users.noreply.github.com>
2021-09-02 16:08:11 +0800

    Update jquery.filer.js

--- src/assets/js/jquery.filer.js
+++ src/assets/js/jquery.filer.js
@@ -972,27 +972,4 @@
 			}
 		}
 	}
-	
-	$('.jFiler-item-thumb-image img').click(function () {
-	    //获取图片路径
-	    var imgsrc = $(this).attr("src");
-	    console.log(imgsrc);
-	    var opacityBottom = '<div class="opacityBottom" style = "display:none"><img class="bigImg" src="' + imgsrc + '"></div>';
-	    $(document.body).append(opacityBottom);
-	    toBigImg();//变大函数
-
-	});
-	
-	function toBigImg() {
-	    $(".opacityBottom").addClass("opacityBottom");//添加遮罩层
-	    $(".opacityBottom").show();
-	    $("html,body").addClass("none-scroll");//下层不可滑动
-	    $(".bigImg").addClass("bigImg");//添加图片样式
-	    $(".opacityBottom").click(function () {//点击关闭
-		$("html,body").removeClass("none-scroll");
-		$(".opacityBottom").remove();
-	    });
-	}
-
-
 })(jQuery);
